b2f6b5d93f6b64977f4461716bd5a9fdfc13eb70
Gabriel de Tassigny <gabriel@multivac.cc>
2015-07-08 18:59:20 +0100

    Less hardcoded values

--- src/player.rb
+++ src/player.rb
@@ -2,20 +2,23 @@ require './src/moveable.rb'
 require './src/bullet.rb'
 
 class Player < Moveable
+  SHIP_SIZE = 48
+
   def initialize
     super
     @image = Gosu::Image.new 'assets/spaceship.png'
+    @speed = 4.5
   end
 
   def move_left
     if @x > 0.0
-      @x -= 4.5
+      @x -= @speed
     end
   end
 
   def move_right
-    if @x < GameWindow::WIDTH - 48
-      @x += 4.5
+    if @x < GameWindow::WIDTH - SHIP_SIZE
+      @x += @speed
     end
   end
 
